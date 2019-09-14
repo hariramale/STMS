f7564e718653e3c0ab46a398cd4e956c04156c08
Hariram Ale Magar <ahariram@Hariram.local>
2019-09-14 21:38:09 +0545

    db-config

--- header.php
+++ header.php
@@ -37,7 +37,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <!--logo start-->
 <div class="brand">
     <a href="index.php" class="logo">
-        RF-Portal
+        STMS
     </a>
     <div class="sidebar-toggle-box">
         <div class="fa fa-bars"></div>
