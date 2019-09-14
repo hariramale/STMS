4c7fc2588dc5b0f4ee92b50e4a74b5234041f07f
Hariram Ale Magar <30772832+hariramale@users.noreply.github.com>
2019-09-14 18:17:48 +0545

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,7 +1,7 @@
 <?php
 $db_server ='127.0.0.1';
 $db_username='root';
-$db_password='root3312';
+$db_password='';
 $database='SajiloDB';
 $con=mysqli_connect($db_server,$db_username,$db_password,$database);
 // Check connection
